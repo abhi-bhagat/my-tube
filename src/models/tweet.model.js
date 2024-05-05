6094a667147a658a22bb8ce43dc4e0e3d375262b
Abhishek Bhagat <40839483+abhi-bhagat@users.noreply.github.com>
2024-05-05 19:02:42 +0530

    add model files

---
+++ src/models/tweet.model.js
@@ -0,0 +1,19 @@
+import mongoose,{Schema} from "mongoose";
+
+
+const tweetSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Tweet = mongoose.model("Tweet".tweetSchema);
